39070b6d155e92efe373a0a65f07340fd8fb9116
Harshit77Nav <harshit77navneet@gmail.com>
2023-05-29 16:57:56 +0530

    angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo';
+  text:string = "Angular Setup";
+  count:number = 0;
+  disable:any = true;
+  show:boolean = true;
+  color:string = 'red';
+
+  getName(val:any){
+    // alert("Working")
+    console.log(val);
+    this.text = val;
+  }
+
+  dis(){
+    this.disable = !this.disable;
+  }
+
+  counter(val:string){
+    switch(val){
+      case 'add':
+        this.count++;
+        break;
+      case 'sub':
+        this.count--;
+        break;
+      default:
+        console.log("error");
+        
+    }
+  }
+
+  changeIf(){
+    this.show=!this.show;
+  }
+
+  changecolor(val:any){
+    this.color = val;
+  }
+
 }
